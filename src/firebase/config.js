76ec3361fc41c08f728cb8cfe1a3e67efc9edb25
sumitbillore-nyt <sumit.billore@nytimes.com>
2026-03-28 01:34:17 +0530

    Securely hidden Firebase keys and updated PlayerDashboard

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_2kHt53Ny4UCWsMRyPzlvDsBgktRc-y4",
-  authDomain: "bcapp-15130.firebaseapp.com",
-  projectId: "bcapp-15130",
-  storageBucket: "bcapp-15130.appspot.com",
-  messagingSenderId: "946729397276",
-  appId: "1:946729397276:web:d11068740200b4962643d9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,7 +17,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-// --- ADD THIS: Secondary instance for Admin tasks ---
+// --- Secondary instance for Admin tasks ---
 const secondaryApp = initializeApp(firebaseConfig, "Secondary");
 export const secondaryAuth = getAuth(secondaryApp);
 
